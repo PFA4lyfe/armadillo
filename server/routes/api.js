69a69aa2abdaf84def94e24d83f93a624f250d66
HM-Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2024-04-06 00:04:40 -0400

    added route api.js

---
+++ server/routes/api.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+      const {data, error} = await supabase
+      .from('kids')
+      .select();
+    
+      if (supabase) {
+        console.log(data);
+      }
+      if (error) {
+        console.log(error);
+      }
+      supabase.from('persons').select()
+        .then(resp => console.log(resp));
+
+      res.status(200).json(data);
+    } catch(error) {
+      console.log('failed')
+      res.status(404).json(error);
+    }
+});
+
+export default router; 
--- server/server.js
+++ server/server.js
@@ -4,42 +4,46 @@ import path from 'path';
 const PORT = 3000;
 const __dirname = import.meta.dirname;
 import supabase from '../server/models/armadilloModel.js';
+import apiRouter from './routes/api.js'; 
+
+//define route handlers
+app.use('/api', apiRouter);
 
 /**
  * handle requests for static files
  */
 app.use(express.static(path.resolve(__dirname, '../src')));
 
-app.get('/', async (req, res) => {
-    // db.query('SELECT * FROM Users;')
-    //   .then(response => console.log(response.rows))
-    //   .then(() => res.status(200).json({a: 1}));
+// app.get('/', async (req, res) => {
+//     // db.query('SELECT * FROM Users;')
+//     //   .then(response => console.log(response.rows))
+//     //   .then(() => res.status(200).json({a: 1}));
 
 
-    try {
-      const {data, error} = await supabase
-      .from('kids')
-      .select();
+//     try {
+//       const {data, error} = await supabase
+//       .from('kids')
+//       .select();
     
-      if (supabase) {
-        console.log(data);
-      }
-      if (error) {
-        console.log(error);
-      }
-      // res.locals.result = await users;
+//       if (supabase) {
+//         console.log(data);
+//       }
+//       if (error) {
+//         console.log(error);
+//       }
+//       // res.locals.result = await users;
 
-      // console.log('result', res.locals.result);
+//       // console.log('result', res.locals.result);
 
-      supabase.from('persons').select()
-        .then(resp => console.log(resp));
+//       supabase.from('persons').select()
+//         .then(resp => console.log(resp));
 
-      res.status(200).json(data);
-    } catch(error) {
-      console.log('failed')
-      res.status(404).json(error);
-    }
-});
+//       res.status(200).json(data);
+//     } catch(error) {
+//       console.log('failed')
+//       res.status(404).json(error);
+//     }
+// });
 
 // catch-all route handler for any requests to an unknown route
 app.use('*', (req, res) => res.sendStatus(404));
