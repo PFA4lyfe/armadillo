49bddaa3828b47a2de76d3c91abc51e8edd8642a
“Chris <chrisr0892@gmail.com>
2024-04-05 21:18:30 -0700

    adjusted app router route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,28 +1,29 @@
 import express from 'express';
 import { Router } from 'express';
-
+import supabase from '../models/armadilloModel.js';
 const router = Router();
-
+console.log('hello there');
 router.get('/', async (req, res) => {
-    try {
-      const {data, error} = await supabase
-      .from('kids')
-      .select();
-    
-      if (supabase) {
-        console.log(data);
-      }
-      if (error) {
-        console.log(error);
-      }
-      supabase.from('persons').select()
-        .then(resp => console.log(resp));
+  console.log('hello');
+  try {
+    const { data, error } = await supabase.from('kids').select();
 
-      res.status(200).json(data);
-    } catch(error) {
-      console.log('failed')
-      res.status(404).json(error);
+    if (supabase) {
+      console.log(data);
     }
+    if (error) {
+      console.log(error);
+    }
+    supabase
+      .from('persons')
+      .select()
+      .then((resp) => console.log(resp));
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.log('failed');
+    res.status(404).json(error);
+  }
 });
 
-export default router; 
+export default router;
