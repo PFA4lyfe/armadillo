d015583b9222cdb4a400442f5cee1541cf3f9564
HM-Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2024-04-07 08:37:36 -0400

    handle function helper function to fetch specfic flight data from API added to Flights Search (tested in flight search reference)

--- src/assets/flightSearchReferece.js
+++ src/assets/flightSearchReferece.js
@@ -1,3 +1,4 @@
+//data fetched from API
 const sampleObj = {
     "meta": {
         "count": 5,
@@ -1859,3 +1860,104 @@ const sampleObj = {
         }
     }
 }
+//function that takes fetched data (obj) and returns matching flights (price, dates, airlines, number of travelers)
+  function arrayofTravelObjects(obj, dep_city, dep_date, dest_city, ret_date, num_travelers) {
+    const airlinesObj = {
+        "3X": "Japan Air Commuter",
+        "6E": "IndiGo",
+        "6J": "Solaseed Air",
+        "7G": "StarFlyer",
+        "9C": "Spring Airlines",
+        "AA": "American Airlines",
+        "AC": "Air Canada",
+        "AI": "Air India",
+        "AK": "AirAsia",
+        "AM": "Aeromexico",
+        "ANA": "All Nippon Airways",
+        "AR": "Aerolineas Argentinas",
+        "AV": "Avianca",
+        "B6": "JetBlue Airways",
+        "BC": "Skymark Airlines",
+        "BR": "EVA Air",
+        "CA": "Air China",
+        "CI": "China Airlines",
+        "CM": "Copa Airlines",
+        "CZ": "China Southern Airlines",
+        "DL": "Delta Air Lines",
+        "EK": "Emirates",
+        "ET": "Ethiopian Airlines",
+        "EY": "Etihad Airways",
+        "F9": "Frontier Airlines",
+        "FJ": "Fiji Airways",
+        "GA": "Garuda Indonesia",
+        "GK": "Jetstar Japan",
+        "GJ": "Loong Air",
+        "HA": "Hawaiian Airlines",
+        "IT": "Tigerair Taiwan",
+        "JL": "Japan Airlines",
+        "KA": "Cathay Dragon",
+        "KE": "Korean Air",
+        "KL": "KLM Royal Dutch Airlines",
+        "KN": "China United Airlines",
+        "LA": "LATAM Airlines",
+        "LH": "Lufthansa",
+        "MK": "Air Mauritius",
+        "MS": "EgyptAir",
+        "MU": "China Eastern Airlines",
+        "NH": "All Nippon Airways",
+        "NK": "Spirit Airlines",
+        "NU": "Japan Transocean Air",
+        "NZ": "Air New Zealand",
+        "OC": "Oriental Air Bridge",
+        "ON": "Nauru Airlines",
+        "OZ": "Asiana Airlines",
+        "PX": "Air Niugini",
+        "PR": "Philippine Airlines",
+        "QF": "Qantas Airways",
+        "QR": "Qatar Airways",
+        "RK": "Ryukyu Air Commuter",
+        "SB": "Aircalin",
+        "SG": "SpiceJet",
+        "SK": "Scandinavian Airlines System (SAS)",
+        "SQ": "Singapore Airlines",
+        "SV": "Saudia",
+        "TG": "Thai Airways International",
+        "TN": "Air Tahiti Nui",
+        "TR": "Scoot",
+        "TV": "Tibet Airlines",
+        "UU": "Air Austral",
+        "VA": "Virgin Australia",
+        "VN": "Vietnam Airlines",
+        "WS": "WestJet",
+        "ZG": "ZIPAIR Tokyo",
+        "ZH": "Shenzhen Airlines"
+      };
+    const result = [];
+    
+    obj.data.forEach(flightOffer => { //note: 'flightOffer' is "data" in embeeded response object
+      const price = flightOffer.price.total * num_travelers; 
+      flightOffer.itineraries.forEach(itinerary => {  //itinerary is "itineraries"
+        itinerary.segments.forEach(segment => { //iterates through "segments" (same name)
+          const departureDate = new Date(segment.departure.at).toISOString().split('T')[0]; //slices time off and leaves just with the date
+          const arrivalDate = new Date(segment.arrival.at).toISOString().split('T')[0];
+          const airlines = airlinesObj[segment.carrierCode]
+       
+          if (segment.departure.iataCode === dep_city && departureDate === dep_date && segment.arrival.iataCode === dest_city && arrivalDate === ret_date) {
+            result.push({ dep_city, dep_date, dest_city, ret_date, price, airlines, num_travelers });
+          }
+        });
+      });
+    });
+    return result;
+  }
+  
+  //sample user input data 
+  const flightData = sampleObj;
+  const dep_city = "LAX"; // Departure city
+  const dest_city = "JFK"
+  const dep_date= "2024-05-15"
+  const ret_date = "2024-05-16"; // Current date
+  const num_travelers = 2;
+    
+  const matchingFlights = arrayofTravelObjects(flightData, dep_city, dep_date, dest_city, ret_date, num_travelers);
+  console.log("Matching Flights:", matchingFlights);
